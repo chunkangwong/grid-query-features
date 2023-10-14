70d3729c3fa2d38ab3ef64a99b6b7260da394a96
ckwong <chunkang.wong94@gmail.com>
2023-10-14 15:50:43 +0800

    Configured for npm publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({
+      include: [path.resolve(__dirname, "src/hooks/useGridQueryFeatures.ts")],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/hooks/useGridQueryFeatures.ts"),
+      name: "useGridQueryFeatures",
+      fileName: (format) => `useGridQueryFeatures.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+});
